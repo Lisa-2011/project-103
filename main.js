ebe21bb3e4db459de410a4209c91adb8621db525
Lisa-2011 <93234101+Lisa-2011@users.noreply.github.com>
2022-09-09 17:33:35 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ function take_snapshot(){
 
 console.log('ml5 version', ml5.version);
 classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/MsmpvPelL/model.json', modelLoaded);
+
+function check(){
+
+    img = document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+
+}
+
+function gotResult(error, results){
+
+    if(error){
+        console.error(error);
+
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_family_member").innerHTML = results[0].label;
+        document.getElementById("result_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+
+}
